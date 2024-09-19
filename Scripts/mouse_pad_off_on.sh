cf52120ba2255262b2bcda7c9a393ebc683dc8e3
BoomTNT29 <gathik.jindal@outlook.com>
2024-09-19 07:36:50 +0530

    mousepad on/off

---
+++ Scripts/mouse_pad_off_on.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the device ID for the touchpad
+device=$(xinput list --name-only | grep -i "touchpad")
+device_id=$(xinput list | grep "$device" | grep -oP 'id=\K\d+')
+
+# Get the property ID for "Enabling it and Disabling it"
+prop_id=$(xinput list-props "$device_id" | grep -i "Device Enabled (" | grep -oP '\(\K\d+')
+prop_val=$(xinput list-props 12 | grep -i "Device Enabled (" | awk '{print $NF}')
+prop_val=$((1 - prop_val))
+
+# Enable tapping
+xinput set-prop "$device_id" "$prop_id" "$prop_val"
+echo "Device ID: $device_id, Property ID: $prop_id, Property Value: $prop_val"
