a1024514b7386218ec57e884cbd0e35f6af5b5c4
BoomTNT29 <gathik.jindal@outlook.com>
2025-01-21 05:54:54 +0530

    update

--- Scripts/increase_volume.sh
+++ Scripts/increase_volume.sh
@@ -8,6 +8,7 @@ notif_id=1000
 if [ "$current_volume" -lt 100 ]; then
     # Increase the volume by 5%
     pactl set-sink-volume @DEFAULT_SINK@ +5%
+		current_volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -oP '\d+(?=%)' | head -1)
 		notify-send -u "low" -t 500 -r $notif_id "Volume" "Volume set at $current_volume%"
 else
     notify-send -u "low" -t 1000 -r $notif_id "Volume" "Volume is already at or above 100%."
